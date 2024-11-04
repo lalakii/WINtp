5b811ec1b26bae4e9ee42ca6f914dcbf7f15a7f1
lalakii <i@lalaki.cn>
2024-11-04 23:06:09 +0800

    :memo: 添加系统服务实现

--- WINtp.cs
+++ WINtp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -7,24 +8,31 @@ using System.Net;
 using System.Net.Sockets;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.ServiceProcess;
 using System.Threading;
 
 [assembly: AssemblyProduct("WINtp")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: AssemblyTitle("简单好用的时间同步小工具")]
 [assembly: AssemblyCopyright("Copyright (C) 2024 lalaki.cn")]
 
-public static class WINtp
+[DesignerCategory("")]
+public class WINtp : ServiceBase
 {
     private static readonly string exePath = typeof(WINtp).Assembly.Location;
     private static readonly ManualResetEvent evt = new(false);
-    private static CancellationTokenSource cts;
-    private static bool autoSyncTime = true;
-    private static bool verbose = false;
+    private CancellationTokenSource cts;
+    private bool autoSyncTime = false;
+    private bool verbose = false;
     private static SystemTime st;
 
-    public static void Main()
+    protected override void OnStart(string[] args)
+    {
+        ThreadPool.QueueUserWorkItem(InitProfile, args);
+    }
+
+    public void InitProfile(object args)
     {
         List<string> NtpServer = ["time.asia.apple.com", "time.windows.com", "rhel.pool.ntp.org"];
         var cfgPath = Path.Combine(Path.GetDirectoryName(exePath), "ntp.ini");
@@ -33,8 +41,8 @@ public static class WINtp
         {
             foreach (var item in File.ReadAllLines(cfgPath))
             {
-                var srv = item.Trim().Replace(" ", "").ToLower();
-                if (!string.IsNullOrEmpty(srv) && !srv.StartsWith("#") && !srv.StartsWith("/"))
+                var srv = (item + "").Trim().Replace(" ", "").ToLower();
+                if (StartsWithoutComment(srv))
                 {
                     if (srv.Contains("="))
                     {
@@ -42,13 +50,13 @@ public static class WINtp
                         {
                             useDefaultNtpServer = false;
                         }
+                        else if (srv.Contains("autosynctime=true"))
+                        {
+                            autoSyncTime = true;
+                        }
                         else if (srv.Contains("verbose=true"))
                         {
                             verbose = true;
-                        }
-                        else if (srv.Contains("autosynctime=false"))
-                        {
-                            autoSyncTime = false;
                         }
                     }
                     else
@@ -68,11 +76,34 @@ public static class WINtp
         using (cts = new CancellationTokenSource())
         {
             NtpServer.ForEach(srv => ThreadPool.QueueUserWorkItem(GetNtpTime, srv));
-            if (!evt.WaitOne(30000))
+            if (!evt.WaitOne(10000))
             {
                 Environment.FailFast(exePath + " 同步时间失败，网络不畅通。");
             }
+            if (args != null)
+            {
+                Thread.Sleep(3000);
+                Stop();
+            }
         }
+    }
+
+    public static void Main(string[] args)
+    {
+        var svc = new WINtp();
+        if (args != null && args.Contains("-k", StringComparer.OrdinalIgnoreCase))
+        {
+            Run(svc);
+        }
+        else
+        {
+            svc.InitProfile(null);
+        }
+    }
+
+    public static bool StartsWithoutComment(string str)
+    {
+        return str != "" && !new char[] { '-', '#', '/', ';', '<', '=', ':' }.Contains(str.First());
     }
 
     // stackoverflow.com/a/3294698/162671
@@ -85,7 +116,7 @@ public static class WINtp
                        ((x & 0xff000000) >> 24)) * 1000;
     }
 
-    private static void GetNtpTime(object server)
+    private void GetNtpTime(object server)
     {
         var date = new DateTime(1900, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
         var data = new byte[48];
@@ -93,7 +124,7 @@ public static class WINtp
         try
         {
             using Socket socket = new(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            socket.SendTimeout = socket.ReceiveTimeout = 3000;
+            socket.SendTimeout = socket.ReceiveTimeout = 5000;
             socket.Connect(new IPEndPoint(Dns.GetHostEntry((string)server).AddressList.ElementAtOrDefault(0), 123));
             socket.Send(data);
             socket.Receive(data);
